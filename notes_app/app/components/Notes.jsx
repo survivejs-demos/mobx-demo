6a345dbe16459c4e9b7d204eae1daa3fe6c7eb0b
Juho Vepsalainen <bebraw@gmail.com>
2015-08-13 10:19:05 +0300

    notes_app - Move to reference based semantics
    This gets rid of some code.

--- notes_app/app/components/Notes.jsx
+++ notes_app/app/components/Notes.jsx
@@ -19,8 +19,8 @@ export default class Notes extends React.Component {
       <li className='note' key={`note${note.id}`}>
         <Note
           task={note.task}
-          onEdit={this.props.onEdit.bind(null, note.id)}
-          onDelete={this.props.onDelete.bind(null, note.id)} />
+          onEdit={this.props.onEdit.bind(null, note)}
+          onDelete={this.props.onDelete.bind(null, note)} />
       </li>
     );
   }
--- notes_app/app/stores/NoteStore.js
+++ notes_app/app/stores/NoteStore.js
@@ -21,35 +21,22 @@ class NoteStore {
   addNote({task}) {
     this.notes.push({id: uuid.v4(), task});
   }
-  editNote(id, task) {
+  editNote(note, task) {
+    if(!note < 0) {
+      return;
+    }
+
+    note.task = task
+  }
+  deleteNote(note) {
     const notes = this.notes;
-    const noteIndex = this.findNote(id);
+    const noteIndex = notes.indexOf(note);
 
     if(noteIndex < 0) {
       return;
     }
 
-    this.notes[noteIndex].task = task
-  }
-  deleteNote(id) {
-    const notes = this.notes;
-    const noteIndex = this.findNote(id);
-
-    if(noteIndex < 0) {
-      return;
-    }
-
-    this.notes.splice(noteIndex, 1);
-  }
-  findNote(id) {
-    const notes = this.notes;
-    const noteIndex = notes.findIndex((note) => note.id === id);
-
-    if(noteIndex < 0) {
-      console.warn('Failed to find note', notes, id);
-    }
-
-    return noteIndex;
+    notes.splice(noteIndex, 1);
   }
 }
 
